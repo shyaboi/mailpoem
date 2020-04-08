671818633d93f460adb7490aea6334c2205c8d81
Ian <ian.shane.sears@gmail.com>
2020-04-08 01:36:47 -0700

    Updates

---
+++ precache-manifest.14f80ebfee7f2065d756e912389bb990.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aac881288289ecebcf5e6136d4ab50e7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0ac4dd6effb1ee2b6ddb",
+    "url": "/static/css/main.8920e894.chunk.css"
+  },
+  {
+    "revision": "d831ac83ce80b40567cc",
+    "url": "/static/js/2.321c8dbb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.321c8dbb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ac4dd6effb1ee2b6ddb",
+    "url": "/static/js/main.410bc245.chunk.js"
+  },
+  {
+    "revision": "f5e72c55853815fadf57",
+    "url": "/static/js/runtime-main.3007fff0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "7a42488d3b6835412833832573cf11a2",
+    "url": "/static/media/mp.7a42488d.mp4"
+  }
+]);
