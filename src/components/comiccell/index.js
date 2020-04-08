25e70940d28b49fae9a3a8a62be4b7443efabe88
Ian <ian.shane.sears@gmail.com>
2020-04-07 23:31:41 -0700

    a thing is working

---
+++ src/components/comiccell/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './style.css';
+
+function video() {
+  return (
+
+
+ 
+    <iframe id="bg" src="https://www.youtube.com/embed/bSuNMVnmEIo?autoplay=1&mute=1&loop=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+  );
+}
+
+export default video;
+
+
+
---
+++ src/components/words/index.js
@@ -0,0 +1,21 @@
+
+
+
+import React from 'react';
+import './style.css';
+
+function words() {
+  return (
+
+
+ 
+    <div class="content">
+    <h1>MaiL Poem</h1>
+    <p>World falling apart. Please take my heart. Everlasting memory, </p>
+  </div>
+  
+
+  );
+}
+
+export default words;
