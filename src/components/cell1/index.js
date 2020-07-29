db7a0d7afad45cc1ccd1b37aa5ac9c3b8f4fafef
Shyaboi <ian.shane.sears@gmail.com>
2020-07-29 00:07:41 -0700

    fixed bored cell

--- src/components/cell1/index.js
+++ src/components/cell1/index.js
@@ -3,37 +3,19 @@ import "./style.css";
 import mp from "./mp.mp4";
 import ReactPlayer from "react-player";
 
-import {
-  Player,
-  ControlBar,
-  ReplayControl,
-  ForwardControl,
-  CurrentTimeDisplay,
-  TimeDivider,
-  PlaybackRateMenuButton,
-  VolumeMenuButton,
-} from "video-react";
 
-export default (props) => {
+function Cell1() {
   return (
     <ReactPlayer
       url="https://youtu.be/RLGqFAS6-eA"
-      volume="0"
       muted
       playing
       loop
       width="100%"
       height="100vh"
-      id="flyWave"
+      id="ember"
     />
-
-    //   <video autoPlay muted loop id="flyWave">
-    //   <source src={mp} type="video/mp4" />
-    // </video>
-
-    //   )
-    // }
-
-    // export default Cell1;
   );
-};
+}
+
+    export default Cell1;
